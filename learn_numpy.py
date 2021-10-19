e00802474aeb5450f1b0a7f0ad022d5526d754ce
Swider <andy.frot@outlook.fr>
2021-10-19 14:05:27 +0200

    Ajouts

--- learn_numpy.py
+++ learn_numpy.py
@@ -10,16 +10,21 @@ ax = plt.subplot()
 tl = pd.read_excel('.\\data\\TL Brawl.xls', index_col=0)
 
 #g = sbn.countplot(x='Univers', data=tl)
+g = sbn.countplot(x='Tier Brawl', data=tl, order=["SS", "S", "A+", "A-", "B", "C+", "C", "C-", "D", "E", "F"])
 
-tl = tl[tl['Univers'] == 'Mario'].T.iloc[:8]
+#tl = tl.dropna(how='any', subset=['Position 64']).T.iloc[9:]
+#tl.loc['Position 64'] = tl.loc['Position 64'] / 12
+#tl.loc['Position Melee'] = tl.loc['Position Melee'] / 26
+#tl.loc['Position Brawl'] = tl.loc['Position Brawl'] / 38
+#tl.loc['Position Smash 4'] = tl.loc['Position Smash 4'] / 55
+#tl.loc['Position Ultimate'] = tl.loc['Position Ultimate'] / 81
 
-col = tl.columns
-
-ax.plot(tl, 'o-')
-ax.set_ylim(39, 0)
-plt.xlabel("Tier lists")
-plt.ylabel("Position")
-plt.legend(col)
+#col = tl.columns
+#ax.plot(tl, 'o-')
+#ax.set_ylim(1, 0)
+#plt.xlabel("Jeux")
+#plt.ylabel("Position globale")
+#plt.legend(col)
 
 #g = bxp(x="Univers", y=dt(2008, 9, 1), data=tl)
 
