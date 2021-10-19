8bbfd58737270f75f76aca86528b50bf9b371fe7
Swider <andy.frot@outlook.fr>
2021-10-19 11:24:16 +0200

    Ajout dans tier list

--- learn_numpy.py
+++ learn_numpy.py
@@ -5,20 +5,22 @@ from seaborn.categorical import boxplot as bxp
 from datetime import datetime as dt
 
 plt.figure(figsize=(12,6))
-#ax = plt.subplot()
+ax = plt.subplot()
 
-tl = pd.read_excel('.\\data\\TL Brawl.xls')
+tl = pd.read_excel('.\\data\\TL Brawl.xls', index_col=0)
 
-#tl = tl.iloc[:10].T.iloc[:8]
-#col = tl.columns
+#g = sbn.countplot(x='Univers', data=tl)
 
-#ax.plot(tl, 'o-')
-#ax.set_ylim(39, 0)
-#plt.xlabel("Tier lists")
-#plt.ylabel("Position")
-#plt.legend(col)
+tl = tl[tl['Univers'] == 'Mario'].T.iloc[:8]
 
-g = sbn.countplot(x='Univers', data=tl)
+col = tl.columns
+
+ax.plot(tl, 'o-')
+ax.set_ylim(39, 0)
+plt.xlabel("Tier lists")
+plt.ylabel("Position")
+plt.legend(col)
+
 #g = bxp(x="Univers", y=dt(2008, 9, 1), data=tl)
 
 plt.show()
