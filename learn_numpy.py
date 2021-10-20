1acb0ae3f1dbac6fc6efb1d64e447f7ab0c8efda
Swider <andy.frot@outlook.fr>
2021-10-20 14:04:42 +0200

    Ajout de Jupyter

--- learn_numpy.py
+++ learn_numpy.py
@@ -20,8 +20,7 @@ g = sbn.countplot(x='Tier', data=tl, order=["SS", "S", "A+", "A-", "B", "C+", "C
 #plt.ylabel("Position")
 # On peut en tirer une chose : quand se sont passé les tier lists
 
-#g = sbn.countplot(x='Univers', data=tl)
-# Combien sont-ils représentés par univers
+#g = sbn.countplot(x='Univers', data=tl) # Combien sont-ils représentés par univers
 
 #tl = tl[(tl['Univers'] == 'Mario') | (tl['Univers'] == 'Pokémon') | (tl['Univers'] == 'Zelda')]
 #g = bxp(x="Univers", y=dt(2013, 4, 25), data=tl)
@@ -36,6 +35,19 @@ g = sbn.countplot(x='Tier', data=tl, order=["SS", "S", "A+", "A-", "B", "C+", "C
 #ax.set_ylim(39, 0)
 #plt.xlabel("Tier lists")
 #plt.ylabel("Position")
+#plt.legend(col)
+
+#tl = tl[tl['Univers'] == 'Mario'].T.iloc[9:-1]
+#tl.loc['Position 64'] /= 12
+#tl.loc['Position Melee'] /= 26
+#tl.loc['Position Brawl'] /= 38
+#tl.loc['Position Smash 4'] /= 55
+#tl.loc['Position Ultimate'] /= 81
+#col = tl.columns
+#ax.plot(tl, 'o-')
+#ax.set_ylim(1.01, -0.01)
+#plt.xlabel("Jeux")
+#plt.ylabel("Position globale")
 #plt.legend(col)
 
 plt.show()
