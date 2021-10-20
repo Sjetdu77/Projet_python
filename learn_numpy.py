30e0ea5d4732ce89b94db9758f2d2ef8cd544ad8
Swider <andy.frot@outlook.fr>
2021-10-20 11:46:00 +0200

    Modifications importantes

--- learn_numpy.py
+++ learn_numpy.py
@@ -10,7 +10,7 @@ ax = plt.subplot()
 
 tl = pd.read_excel('.\\data\\TL Brawl.xls', index_col=0)
 
-#g = sbn.countplot(x='Tier Brawl', data=tl, order=["SS", "S", "A+", "A-", "B", "C+", "C", "C-", "D", "E", "F"])
+g = sbn.countplot(x='Tier', data=tl, order=["SS", "S", "A+", "A-", "B", "C+", "C", "C-", "D", "E", "F"])
 # Combien y a-t-il de personnages par tier ?
 
 #tl = tl.T.iloc[:8]
@@ -20,7 +20,8 @@ tl = pd.read_excel('.\\data\\TL Brawl.xls', index_col=0)
 #plt.ylabel("Position")
 # On peut en tirer une chose : quand se sont passé les tier lists
 
-#g = sbn.countplot(x='Univers', data=tl) # Combien sont-ils représentés par univers
+#g = sbn.countplot(x='Univers', data=tl)
+# Combien sont-ils représentés par univers
 
 #tl = tl[(tl['Univers'] == 'Mario') | (tl['Univers'] == 'Pokémon') | (tl['Univers'] == 'Zelda')]
 #g = bxp(x="Univers", y=dt(2013, 4, 25), data=tl)
@@ -36,18 +37,5 @@ tl = pd.read_excel('.\\data\\TL Brawl.xls', index_col=0)
 #plt.xlabel("Tier lists")
 #plt.ylabel("Position")
 #plt.legend(col)
-
-tl = tl[tl['Univers'] == 'Mario'].T.iloc[9:-1]
-tl.loc['Position 64'] /= 12
-tl.loc['Position Melee'] /= 26
-tl.loc['Position Brawl'] /= 38
-tl.loc['Position Smash 4'] /= 55
-tl.loc['Position Ultimate'] /= 81
-col = tl.columns
-ax.plot(tl, 'o-')
-ax.set_ylim(1.01, -0.01)
-plt.xlabel("Jeux")
-plt.ylabel("Position globale")
-plt.legend(col)
 
 plt.show()
